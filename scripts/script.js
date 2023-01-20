36bcf5e8aa362ecf23d9b43543bc541a02857afd
Heath <jimmyhj9@gmail.com>
2023-01-20 13:57:04 -0600

    implemented light/dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,50 @@
+let dark = false;
+let a1 = document.getElementById('a1');
+let a2 = document.getElementById('a2');
+let a3 = document.getElementById('a3');
+let a4 = document.getElementById('a4');
+const arr = [a1, a2, a3, a4];
+
+// function for light/dark modes
+function switchMode(event) {
+    if(dark) { //switches to (default) light mode
+        event.target.style.backgroundColor = '#FFF';
+        event.target.style.color = '#000';
+        event.target.style.border = '1px solid black';
+        event.target.innerHTML = 'Dark mode';
+
+        // changing anchor font color because its stubborn
+        arr.forEach(anchor => {
+            anchor.style.color = '#000';
+        });
+
+        document.body.style.backgroundColor = '';
+        document.body.style.color = '#000';
+        dark = false;
+    }
+    else { //switches to dark mode
+        event.target.style.backgroundColor = '#405060';
+        event.target.style.color = '#FFF';
+        event.target.style.border = '1px solid white';
+        event.target.innerHTML = 'Light mode';
+        
+        arr.forEach(anchor => {
+            anchor.style.color = '#FFF';
+        });
+
+        document.body.style.background = '#405060';
+        document.body.style.color = '#FFF';
+        dark = true;
+    }
+
+    return dark;
+}
+
+const mode = document.getElementById('mode');
+mode.onclick = switchMode;
+
+
+
 let now = new Date();
 let birth = new Date(1998, 5, 10);
 let myAge = Math.floor((now - birth) / 31536000000);
@@ -12,6 +59,8 @@ if(myAge < 26) {
 else {
     occ.innerHTML = 'employed as a web developer';
 }
+
+
 
 //generating random happy emoji for footer
 const foot = document.getElementById('f1');
--- scripts/script2.js
+++ scripts/script2.js
@@ -1,9 +1,53 @@
+let dark = false;
+let a1 = document.getElementById('a1');
+let a2 = document.getElementById('a2');
+let a3 = document.getElementById('a3');
+let a4 = document.getElementById('a4');
+const arr = [a1, a2, a3, a4];
+
+// function for light/dark modes
+function switchMode(event) {
+    if(dark) { //switches to (default) light mode
+        event.target.style.backgroundColor = '#FFF';
+        event.target.style.color = '#000';
+        event.target.style.border = '1px solid black';
+        event.target.innerHTML = 'Dark mode';
+
+        // changing anchor font color because its stubborn
+        arr.forEach(anchor => {
+            anchor.style.color = '#000';
+        });
+
+        document.body.style.backgroundColor = '';
+        document.body.style.color = '#000';
+        dark = false;
+    }
+    else { //switches to dark mode
+        event.target.style.backgroundColor = '#405060';
+        event.target.style.color = '#FFF';
+        event.target.style.border = '1px solid white';
+        event.target.innerHTML = 'Light mode';
+        
+        arr.forEach(anchor => {
+            anchor.style.color = '#FFF';
+        });
+
+        document.body.style.background = '#405060';
+        document.body.style.color = '#FFF';
+        dark = true;
+    }
+
+    return dark;
+}
+
+const mode = document.getElementById('mode');
+mode.onclick = switchMode;
+
+
+
 //generating random happy emoji for footer
 const foot = document.getElementById('f2');
 const emojis = ['😀', '😃', '😄', '😁', '😆', '🙂', '🙃', '😉', '😗', '😚', '😙', '🤗', '😬'];
 const randomEmote = emojis[Math.floor(Math.random() * emojis.length)];
 
 foot.innerHTML += randomEmote;
-
-const button = document.querySelector('button');
-button.style.color = 'blue';
